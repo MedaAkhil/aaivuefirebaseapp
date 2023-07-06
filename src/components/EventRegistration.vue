c65c90bfd6573b1ff21b3125f96d1490a65d600a
unknown <medaakhilaeshchowdary@gmail.com>
2023-07-06 23:37:08 +0530

    first commit

---
+++ src/components/EventRegistration.vue
@@ -0,0 +1,229 @@
+<template>
+  <center>
+    <div>
+    <h2>Event Registration</h2>
+    <div class="card">
+            <span class="card_title">Register</span>
+            <p class="card_content">Register and attend the event</p>
+            <div class="card_form">
+    <form @submit.prevent="register">
+      <!-- <label for="name">Name:</label>
+      <input type="text" id="name" v-model="name" required>
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email" required>
+      <button type="submit">Register</button> -->
+      <input type="text" id="name" v-model="name" placeholder="Enter Student Name"><br>
+      <input type="text" id="rollno" v-model="rollno" placeholder="Enter Roll NO"><br>
+      <input type="text" id="email" v-model="email" placeholder="Enter Collage Email ID"><br>
+      <input type="text" id="branch" v-model="branch" placeholder="Enter Branch"><br>
+      <input type="text" id="section" v-model="section" placeholder="Enter Section"><br>
+      <input type="text" id="year" v-model="year" placeholder="Enter Year"><br>
+      <input type="tel" id="phno" v-model="phno" placeholder="Enter WhatsApp Number"><br>
+      <input type="submit">
+    </form>
+  </div>
+        </div>
+  </div>
+  </center>
+</template>
+
+<script>
+import { firestore } from '@/firebase'
+
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      rollno: '',
+      branch: '',
+      section:'',
+      year:'',
+      phno:''
+    }
+  },
+  methods: {
+    register() {
+      console.log("hello");
+      firestore.collection('registrations').add({
+        name: this.name,
+        email: this.email,
+        rollno: this.rollno,
+        branch: this.branch,
+        section:this.branch,
+        year:this.year,
+        phno:this.phno
+      })
+      this.name = ''
+      this.email = ''
+      this.rollno = ''
+      this.branch= ''
+      this.section=''
+      this.year=''
+      this.phno=''
+    },
+  },
+  mounted() {
+    // Add the first script tag
+    const script1 = document.createElement('script');
+    script1.src = 'https://unpkg.com/aos@2.3.1/dist/aos.js';
+    document.body.appendChild(script1);
+
+    // Add the second script tag
+    const script2 = document.createElement('script');
+    script2.innerHTML = "AOS.init();"; // Add any additional initialization code if required
+    document.body.appendChild(script2);
+    AOS.init();
+  },
+}
+</script>
+
+
+<style>
+/* .eventregformsec {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+
+}
+
+.formdiv {
+    background-color: pink;
+    padding: 30px;
+    border-radius: 10px;
+}
+
+.eventform input {
+    font-size: 2rem;
+    margin: 1px;
+    border: 0 0 1px 0 solid black;
+    margin-top: 0;
+    padding: 5px;
+    border-radius: 10px;
+} */
+h2{
+  margin: 4% 0 2% 0;
+}
+h2{
+  color: white;
+}
+.eventregformsec {
+    width: 100%;
+    height: 120vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.card {
+    width: 50%;
+    padding: 500px;
+    padding: 100px 15px;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 16px;
+    background: #12130c;
+    border-radius: 20px;
+    backdrop-filter: blur(15px);
+}
+
+/* .card > *{
+    margin: 0;
+} */
+.card_title {
+    font-size: 23px;
+    font-weight: 900;
+    color: #fff;
+}
+
+.card_content {
+    font-size: 13px;
+    line-height: 18px;
+    color: #fff;
+}
+
+.card_form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+
+}
+
+.card_form input {
+    /* margin-top: 5px; */
+    outline: 0;
+    background: rgb(255, 255, 255);
+    box-shadow: transparent 0px 0px 0px 1px inset;
+    padding: 0.9em;
+    border-radius: 14px;
+    width: 350px;
+    border: 1px solid #333;
+    margin: 10px;
+    color: black;
+}
+
+.card_form button {
+    border: 0;
+    background: #111;
+    color: #fff;
+    padding: 0.68em;
+    border-radius: 14px;
+    font-weight: bold;
+}
+
+.sign-up {
+    opacity: 0.8;
+}
+
+@media (max-width:920px) {
+    .card {
+        width: 80%;
+    }
+}
+
+@media (max-width:550px) {
+    .card_form input {
+        width: 250px;
+        padding: 9px;
+    }
+}
+
+@media (max-width:420px) {
+    .card_form input {
+        width: 210px;
+        padding: 8px;
+    }
+}
+
+@media (max-width:325px) {
+    .card_form input {
+        width: 180px;
+        padding: 9px;
+    }
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/HomePage.vue
@@ -0,0 +1,613 @@
+<template>
+    
+    <main>
+        <section id="hero" data-aos="zoom-in-up">
+            <h1>Welcome to the Anurag Artificial</h1>
+            <h1 class="eff">Intelligence </h1>
+            <p>Unleash the power of AI with us.</p>
+        </section>
+    </main>
+
+
+
+    <section>
+        <div class="grand_parent-goal">
+
+            <div class="parents-goal">
+                <div class="child-goal">
+                    <div class="img-1" data-aos="zoom-in-up"></div>
+                </div>
+                <div class="child-goal">
+                    <div class="img-1-text">
+                        <h1>About us</h1>
+                        <p>A club of innovation and learning where like-minded individuals come together to explore the
+                            fascinating world of AI. Our club is a place where curiosity and creativity meet
+                            cutting-edge
+                            technology, and where our members are encouraged to push the boundaries of what's possible.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="parents-goal">
+
+                <div class="child-goal">
+                    <div class="img-2" data-aos="zoom-in-up"><img width="90%" src="https://openailabsprodscus.blob.core.windows.net/private/user-V6CbHho6U2HBZRPs3fK2dvIj/generations/generation-Dbn5O34j7FerslYlUaJGglJo/image.webp?st=2023-07-05T00%3A36%3A22Z&se=2023-07-05T02%3A34%3A22Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-07-04T14%3A29%3A09Z&ske=2023-07-11T14%3A29%3A09Z&sks=b&skv=2021-08-06&sig=ym6AdpD96R7/qSDAo1w1IV%2BlCmPleBbjfhnJqhwLGT8%3D" alt=""></div>
+                </div>
+                <div class="child-goal">
+                    <div class="img-2-text">
+                        <h1>Our Vision</h1>
+                        <p> A vibrant community where innovative minds come together to explore the limitless possibilities of artificial intelligence. Our vision is to create a collaborative environment that empowers individuals to dive deep into the world of machine learning, fostering creativity, knowledge sharing, and technological breakthroughs.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="grand_parent">
+            <h1>- Club Goals -</h1>
+            <div class="parents">
+                <div class="child" data-aos="zoom-in-up">
+                    <h3>Learning from industry leaders and guest speakers.</h3>
+                    <p>We invite industry leaders and guest speakers to share their experiences about AI with our
+                        members. This provides us with insights into the industry and helps us stay up-to-date.</p>
+                </div>
+                <div class="child" data-aos="zoom-in-up">
+                    <h3>Providing mentorship and guidance to newcomers.</h3>
+                    <p>We mentor and guide newcomers who are interested in learning about AI. We offer support to help
+                        them Advance. Our members are always ready to answer questions and offer insights to those just
+                        starting their journey in AI.</p>
+                </div>
+                <div class="child" data-aos="zoom-in-up">
+                    <h3>Collaborating on AI-based projects and initiatives.</h3>
+                        <p>Members of the club work together to develop projects that utilize AI. We form teams to build
+                            applications, develop algorithms, and explore new research areas in the field.</p>
+                </div>
+                <div class="child" data-aos="zoom-in-up">
+                    <h3>Networking with professionals and experts in the field.</h3>
+                    <p>We connect with professionals and experts in the field of AI to learn from their experiences,
+                        gain insights into the industry, and create potential career opportunities.</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    
+  </template>
+  
+  <script>
+  
+  export default {
+    
+    
+mounted() {
+// Add the first script tag
+const script1 = document.createElement('script');
+script1.src = 'https://unpkg.com/aos@2.3.1/dist/aos.js';
+document.body.appendChild(script1);
+
+// Add the second script tag
+const script2 = document.createElement('script');
+script2.innerHTML = "AOS.init();"; // Add any additional initialization code if required
+document.body.appendChild(script2);
+}}
+  </script>
+  
+  <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Open Sans", sans-serif;
+}
+
+
+.eff {
+    font-size: 25px;
+    overflow: hidden;
+    position: relative;
+    margin: 10px 0 0 0;
+    height: 100px;
+    color: white;
+}
+
+.eff::after {
+    content: 'Club!';
+    display: inline-block;
+    animation: animation 4.7s steps(9) infinite;
+    width: 0ch;
+    color: lightseagreen;
+}
+
+.eff::before {
+    display: inline-block;
+    content: "";
+    width: 6px;
+    height: 3vh;
+
+    background-color: white;
+    position: absolute;
+    right: 0;
+    animation: blinking linear alternate infinite;
+}
+
+@media (min-width:990px) {
+    .eff::before {
+        height: 50px;
+    }
+}
+
+@keyframes animation {
+    25% {
+        content: ' Club!';
+        width: 4.5ch;
+    }
+
+    40% {
+        width: 0ch;
+    }
+
+    75% {
+        content: '';
+    }
+
+    90% {
+        width: 0ch;
+    }
+}
+
+@keyframes blinking {
+    100% {
+        opacity: 0;
+    }
+}
+
+body {
+    background-color: #000;
+    background-repeat: no-repeat;
+    background-size: 350vh 45%;
+    background-position: top center;
+}
+
+li {
+    list-style: none;
+}
+
+a {
+    text-decoration: none;
+    color: #fff;
+    /* font-size: 1.6rem; */
+}
+
+a:hover {
+    color: orange;
+}
+
+section#hero {
+    height: 95vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: left;
+    color: #fff;
+}
+
+#hero h1 {
+    font-size: 4rem;
+    color: white;
+}
+
+.img-1 {
+    box-sizing: border-box;
+    width: 80%;
+    /* margin: 9% 0 0 9%; */
+    background-size: contain;
+    aspect-ratio: 1;
+    /* background-image: url(assets/Snapinsta.app_337567273_575169784591124_8759515688175258592_n_1080.jpg); */
+    background-repeat: no-repeat;
+}
+
+.img-1-text {
+    color: #088178;
+    font-size: 3vh;
+    align-self: center;
+    box-sizing: border-box;
+    margin: 0;
+    text-align: left;
+    width: 80%;
+}
+
+.img-2 {
+    box-sizing: border-box;
+    width: 80%;
+    /* margin: 9% 0 0 9%; */
+    background-size: contain;
+    aspect-ratio: 1;
+    background-repeat: no-repeat;
+}
+
+.img-2-text {
+    color: #088178;
+    font-size: 3vh;
+    box-sizing: border-box;
+    margin: 4%;
+    text-align: left;
+    width: 90%;
+}
+
+.grand_parent-goal {
+    width: 100%;
+    height: fit-content;
+    background: #dfe6e9;
+    color: #fff;
+    padding: 10% 0 10% 0;
+}
+
+h1 {
+    font-size: 4rem;
+    padding: 9px 0;
+    text-align: center;
+    color: #000;
+}
+
+.child-goal p {
+    margin-top: 10%;
+    text-align: left;
+}
+
+.parents-goal {
+    width: 100%;
+    height: auto;
+    margin: auto;
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    transition: all 0.1 linear;
+}
+
+.child-goal {
+    height: fit-content;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1 1 500px;
+}
+
+@media (max-width: 768px) {
+    .grand_parent-goal {
+        height: auto;
+    }
+}
+
+@media (min-width:768) {
+    .grand_parent-goal {
+        display: none;
+    }
+}
+
+.grand_parent {
+    width: 100%;
+    height: fit-content;
+    background: #dfe6e9;
+    color: #fff;
+    padding: 10% 0 10% 0;
+}
+
+.child {
+    height: 320px;
+    padding: 1%;
+    background-color: grey;
+    flex: 1 1 200px;
+    margin: 30px;
+    border-radius: 5%;
+}
+
+.child h1 {
+    font-size: 4rem;
+    padding: 20px 0;
+    margin-top: 10px;
+    text-align: center;
+    color: #000;
+}
+
+.child p {
+    margin-top: 20%;
+    text-align: justify;
+}
+
+.parents {
+    width: fit-content;
+    height: auto;
+    margin: auto;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    transition: all 0.1 linear;
+    padding: 10px;
+}
+
+@media (max-width: 768px) {
+    .child {
+        margin-top: 10%;
+        padding: 2%;
+    }
+
+    .child p {
+        margin: 10px;
+    }
+}
+
+@media (max-width: 768px) {
+    .grand_parent {
+        height: auto;
+    }
+
+}
+
+@media (max-width: 630px) {
+    .img-1-text p {
+        font-size: 20px;
+    }
+
+    .img-2-text p {
+        font-size: 20px;
+    }
+}
+
+@media (max-width: 992px) {
+    #hero h1 {
+        font-size: 2rem;
+        margin-bottom: 1rem;
+        /* margin-top: 60%; */
+        /* width: 140%; */
+        margin-left: 10px;
+        text-align: left;
+    }
+}
+
+@media (max-width: 576px) {
+    /* .dropdown_menu{
+          left: 2rem;
+          width: unset;
+        } */
+}
+
+html {
+    font-size: 62.5%;
+}
+
+body {
+    font-family: sans-serif;
+    font-size: 1.6rem;
+    background-color: #12131c;
+}
+
+i {
+    font-size: 2.4rem;
+}
+
+.footer {
+    box-sizing: border-box;
+    padding-block: 5rem;
+    bottom: 0;
+    margin-bottom: 0;
+    background-color: #151823;
+    color: #666873;
+}
+
+.footer_container {
+    max-width: 116rem;
+    margin: auto;
+    padding: 0 1.5rem;
+}
+
+.footer_top {
+    display: grid;
+    grid-template-columns: repeat (auto-fit, minmax (20rem, 1fr));
+    row-gap: 2rem;
+}
+
+.footer_title {
+    font-size: 1.8rem;
+    color: #fff;
+    font-weight: 500;
+    margin-bottom: 2rem;
+}
+
+.footer_list {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+}
+
+.footer_list-item {
+    list-style: none;
+}
+
+.footer_list-link {
+    text-decoration: none;
+    color: inherit;
+    transition: color 0.25s;
+}
+
+.footer_list-link:hover {
+    color: #fff;
+}
+
+.company_info {
+    grid-column: 1/-1;
+    display: flex;
+    flex-direction: column;
+    padding-right: 5rem;
+    gap: 1.5rem;
+}
+
+.company_logo {
+    color: #fff;
+}
+
+.company_info ul {
+    flex-direction: row;
+}
+
+.copyright {
+    margin-top: auto;
+}
+
+
+
+header {
+    /* position: relative; */
+    padding: 0 2rem;
+}
+
+.navbar {
+    width: 100%;
+    height: 60px;
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    z-index: -1;
+    justify-content: space-between;
+}
+
+.navbar .logo a {
+    font-size: 3rem;
+    font-weight: bold;
+}
+
+.navbar .links {
+    display: flex;
+    gap: 2rem;
+}
+
+.navbar .toggle_btn {
+    color: #fff;
+    font-size: 2.9rem;
+    cursor: pointer;
+    display: none;
+}
+
+.action_btn {
+    background-color: orange;
+    color: #fff;
+    padding: 0.5rem 1rem;
+    border: none;
+    outline: none;
+    border-radius: 20px;
+    font-size: 0.8rem;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+.action_btn:hover {
+    scale: 1.05;
+    color: #fff;
+}
+
+.action_btn:active {
+    scale: 0.95;
+}
+
+.dropdown_menu {
+    z-index: 100;
+    display: none;
+    position: absolute;
+    right: 2rem;
+    top: 60px;
+    height: 0;
+    width: 300px;
+    background: rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(15px);
+    border-radius: 10px;
+    overflow: hidden;
+    transition: height 0.2s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.dropdown_menu.open {
+    height: 190px;
+}
+
+.dropdown_menu li {
+    padding: 0.7rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.dropdown_menu .action_btn {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+@media (max-width: 992px) {
+
+    .navbar .links,
+    .navbar .action_btn {
+        display: none;
+    }
+
+    .navbar .toggle_btn {
+        display: block;
+    }
+
+    .dropdown_menu {
+        display: block;
+    }
+}
+
+@media (max-width: 576px) {
+    .dropdown_menu {
+        left: 2rem;
+        width: unset;
+    }
+}
+
+
+/* #hero .eff{
+    overflow: hidden;
+    border-right: .6rem solid white;
+    white-space: nowrap;
+    margin: 0 auto;
+    letter-spacing: .15rem;
+    animation: 
+    typing 3s steps(40,end),
+    blink-caret .75s step-end infinite;
+}
+@keyframes typing{
+    from { width: 0}
+    to{ width: 100% }
+}
+@keyframes blink-caret{
+    from, to{border-color: transparent }
+    50% {border-color: orange }
+                } */
+/* .child-goal:first-child {
+          background: #dfe6e9;
+        }
+        .child-goal:last-child {
+            background: #dfe6e9;
+        } */
+</style>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
